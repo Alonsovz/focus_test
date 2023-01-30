c504c6886e91bc74004de5d1292c14229734f834
DANIEL HERNANDEZ <DHERNANDEZ@EDESALCV.local>
2023-01-30 11:36:36 -0600

    cambios token

--- focus_test/src/app/services/contacts.service.ts
+++ focus_test/src/app/services/contacts.service.ts
@@ -5,23 +5,38 @@ import { map, Observable } from 'rxjs';
 import { Contacts } from '../models/contacts';
 import { GlobalService } from './global.service';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'Zoho-oauthtoken 1000.bb79726920428665c774a985ef669e2e.7d48433f91b86e7ad5de0a063281b41f'
-  })
 
-};
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor(private http: HttpClient, private router: Router, private api: GlobalService) { }
+  token!: string;
+
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Zoho-oauthtoken ' + this.token
+    })
+
+  };
+
+
+
+  constructor(private http: HttpClient, private router: Router, private api: GlobalService) {
+    this.api.generate_token().subscribe({
+      next: (data) => {
+        this.token = data;
+      }
+    })
+  }
 
   saveContact(contact: Contacts) {
-    return this.http.post('https://www.zohoapis.com/crm/v2/Contacts', { 'data': [contact] }, { headers: httpOptions.headers }).pipe(map(data => data as any[]));
+    return this.http.post('https://www.zohoapis.com/crm/v2/Contacts', { 'data': [contact] }, { headers: this.httpOptions.headers }).pipe(map(data => data as any[]));
   }
 
 
